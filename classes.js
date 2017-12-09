682709a6f7b102c89ae60cfe81227a17b9ec5772
Jaylan Jones <jaylan@DevMountains-MacBook-Pro.local>
2017-12-09 11:49:19 -0600

    completed classes problem set

--- classes.js
+++ classes.js
@@ -21,7 +21,17 @@ Each employee can:
 
 call your class Employee and receive all the data in the constructor in the order listed
 */
-
+class Employee {
+    constructor(first_name,last_name,email,age){
+        this.first_name= first_name;
+        this.last_name= last_name;
+        this.email= email;
+        this.age= age;
+    }
+    makeWidget(){
+        return this.first_name + " "+this.last_name + " Widget";
+    }
+}
 
 
 /*
@@ -39,11 +49,21 @@ They can (methods) :
 
 call your class Manager
 
-*/
-
-
-
-
+*/ 
+// HAD TO REEEALLLY LOOK THIS UP W EXTENDS && SUPER ASK QUESTIONS ABOUT THIS
+class Manager extends Employee{
+    constructor(first_name,last_name,email,age){
+        super(first_name,last_name,email,age);
+        this.reports=[];
+    }
+    hire(employee){
+        this.reports.push(employee);
+    }
+    fire(index){
+        this.reports.splice(index,1);
+    }
+}
+// HAD TO REEEALLLY LOOK THIS UP W EXTENDS && SUPER ASK QUESTIONS ABOUT THIS
 
 /*
 Manager for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
@@ -64,7 +84,34 @@ Everytime they fire an employee they get $100 added to their bonus.
 
 call your class ProgressiveManager
 */
-
+class ProgressiveManager extends Manager{
+    constructor(first_name,last_name,email,age){
+        super(first_name,last_name,email,age);
+        this.title= "Not a manager";
+        this.bonus= 0;
+    }
+    hire(employee) {
+        this.reports.push(employee);
+        if (this.reports.length <= 3) {
+            return this.title = "Barely Manager"
+        } else if (this.reports.length <= 10) {
+            return this.title = "Mostly Manager"
+        }
+        else if (this.reports.length <= 50) {
+            return this.title = "Manager"
+        }
+        else if (this.reports.length <= 100) {
+            return this.title = "Manager Plus"
+        }
+        else if (this.reports.length >= 101) {
+            return this.title = "Bestest Manager"
+        }
+    }
+    fire(index){
+        this.reports.splice(index,1);
+        this.bonus+=100;
+    }
+}
 
 
 
@@ -90,3 +137,25 @@ It can :
       It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
 
 */
+
+//walked through, revisit
+class Machine {
+    constructor(){
+        this.widgets_made_count = 0;
+        this.wear_and_tear_count = 0;
+        this.needs_reboot = false;
+    }
+    makeWidgets(num){
+        this.widgets_made_count+=num;
+        this.wear_and_tear_count= this.widgets_made_count/50;
+    }
+    fixMachine(){
+        this.needs_reboot=true;
+    }
+    reboot(){
+        return()=>{
+            this.wear_and_tear_count-=10;
+            this.needs_reboot = false
+        }
+    }
+}
